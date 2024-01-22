7dd9a9b2e3500c326b0d6966bb0783bd77913d9e
sulphurninja <sulphurninja@gmail.com>
2024-01-22 17:33:50 +0530

    First skeleton

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = nextConfig
 
 module.exports={
   env:{
-    "BASE_URL": "http://localhost:3000",
+    "BASE_URL": "https://funroulette.vercel.app",
     "MONGODB_URL":"mongodb+srv://aditya4sure:funroulette@testing.in4i0an.mongodb.net/?retryWrites=true&w=majority",
     "ACCESS_TOKEN_SECRET":"iuewugshdbniuewhdsnf8iuc8we7yhf83iwiuhf92hend9od2mo3wheniuw",
     "REFRESH_TOKEN_SECRET":"382ewhnd87iu32hw9r8hd329wejdio328h7wgrbf8d73whenrd32wunm32uid"
