6ead46993d444915eece998ad88e1e4b6f9e4765
Aditya Pingle <95356721+sulphurninja@users.noreply.github.com>
2024-02-06 00:41:00 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,52 +6,14 @@ import { useRouter } from "next/router";
 import { useContext, useState } from 'react';
 
 export default function Login() {
-  const initialState = { userName: "", password: "" };
-  const [userData, setUserData] = useState(initialState);
-  const { userName, password } = userData;
+
   const { state = {}, dispatch } = useContext(DataContext);
-  const { auth = {} } = state;
   const router = useRouter();
 
-  const handleChangeInput = (e) => {
-    const { name, value } = e.target;
-    setUserData({ ...userData, [name]: value });
-  };
 
+  const handleSubmit = () => {
 
-  const handleSubmit = async e => {
-    e.preventDefault()
-
-    const res = await postData('auth/login', userData)
-
-    if (res.error) {
-      window.location.reload();
-      return;
-    }
-
-    dispatch({
-      type: 'AUTH', payload: {
-        token: res.access_token,
-        user: res.user
-      }
-    })
-
-    Cookie.set('refreshtoken', res.refresh_token, {
-      path: '/api/auth/accessToken',
-      expires: 7
-    })
-
-    localStorage.setItem('firstLogin', true)
-
-    // check if user has admin privileges
-    if (res.user.role === 'admin') {
-      router.push("/admin");
-    } else {
-      router.push("/Game");
-    }
-    if (res.user.role === 'subadmin') {
-      router.push("/subadmin")
-    }
+    router.push("/Game");
   }
   return (
     <div className='bg-black w-full flex justify-center  h-screen'>
@@ -62,16 +24,13 @@ export default function Login() {
       </Head>
 
       <img src='/login.png' className='md:w-auto w-full' alt='Fun Roulette GIF' />
-      <form
+      <div
         className=" "
-        onSubmit={handleSubmit}
       >
         <input
           className="shadow appearance-none mt-[40%] -ml-[57%]  rounded w-56 text-lg p-2 bg-white text-black absolute  leading-tight focus:outline-none focus:shadow-outline"
           id="username"
           name="userName"
-          value={userName}
-          onChange={handleChangeInput}
           type="text"
           placeholder="Username"
         />
@@ -80,17 +39,16 @@ export default function Login() {
           id="password"
           type="password"
           name="password"
-          value={password}
-          onChange={handleChangeInput}
           placeholder="******************"
         />
         <button
+        onClick={handleSubmit}
           className=" hover:bg-[#ae1536]  hover:opacity-35 w-36 h-12 mt-[50%] ml-[-42%] text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline absolute"
           type="submit"
         >
 
         </button>
-      </form>
+      </div>
     </div>
   );
 }
